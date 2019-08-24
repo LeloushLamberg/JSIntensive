0632f86c451f958ae44fe0227374ab59922b15a7
LeloushLamberg <egor.bil.1987@yandex.ru>
2019-08-25 06:27:09 +0800

    creat new folder creat mains files connection main.js

---
+++ first_lesson/main.js
@@ -0,0 +1,8 @@
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+alert ("Привет всем! Это моё первое задание");
+console.log ("Привет всем! Это моё первое задание");
