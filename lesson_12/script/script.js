b9a3df49313981bf10a1cc77ea10ba2ce7d08a7a
LeloushLamberg <egor.bil.1987@yandex.ru>
2019-11-21 20:39:07 +0800

    Таймер сделан буду править

--- lesson_12/script/script.js
+++ lesson_12/script/script.js
@@ -1,12 +1,35 @@
-window.addEventListener(`DOMContentLoaded`, function (){
+window.addEventListener(`DOMContentLoaded`, function () {
   'use strict';
 
-  function countTimer(deadline){
-    const hours,
-      minutes,
-      seconds,
-      dateStop,
-      dateNow
-  }
+  function countTimer(deadline) {
+    const timerHours = document.querySelector(`#timer-hours`),
+      timerMinutes = document.querySelector(`#timer-minutes`),
+      timerSeconds = document.querySelector(`#timer-seconds`),
+      dateStop = new Date(deadline).getTime(),
+      dateNow = new Date().getTime();
+
+    function getTimeRemaining() {
+      timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor(((timeRemaining - seconds) / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+      return {
+        hours,
+        minutes,
+        seconds
+      };
+    }
+
+    function updateClock() {
+
+      const timer = getTimeRemaining();
+      timerHours.textContent = timer.hours;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+    };
+  };
+
+  // countTimer(`01 december 2019`)
+  setInterval(countTimer, 1000, `01 december 2019`);
 
 });
