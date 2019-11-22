639c91f80b0ac098bda5f324bc7c287af779a6c5
LeloushLamberg <egor.bil.1987@yandex.ru>
2019-11-22 23:19:27 +0800

    Другое решение - не решает проблемы

--- lesson_12/additional_task/index.js
+++ lesson_12/additional_task/index.js
@@ -2,88 +2,95 @@ window.addEventListener(`DOMContentLoaded`, function () {
   'use strict';
 
   
-  
-  const dateNow = new Date().getTime();
-  
+  let dateNow = new Date()
   const timeOfDay = document.querySelector(`#time-of-day`);
   const timeHours = document.querySelector(`#time-hours`);
   const timeMinutes = document.querySelector(`#time-minutes`);
   const timeSeconds = document.querySelector(`#time-seconds`);
   const timing = document.querySelector(`#timing`);
   const countDay = document.querySelector(`#count-day`);
-
-  function getClock() {
-    const timeNow = dateNow / 1000;
-    const seconds = Math.floor(timeNow % 60);
-    const minutes = Math.floor(((timeNow - seconds) / 60) % 60);
-    const hours = Math.floor((timeNow / 60 / 60) % 24);
-    return {
-      timeNow,
-      hours,
-      minutes,
-      seconds
-    };
-  };
- 
-  getClock();
-  function getTimeOfDay (){
-    const clock = getClock();
-    if (clock.hours > 18){
-      timeOfDay.textContent = `ый вечер`;
-    }
-    if (clock.hours > 12 && clock.hours < 18){
-      timeOfDay.textContent = `ый день`;
-    }
-    if (clock.hours > 6 && clock.hours < 12){
-      timeOfDay.textContent = `ое утро`;
-    }else{
-      timeOfDay.textContent = `ой ночи`;
-    }
-  }
+  let hours = dateNow.getHours()
+  const minutes = dateNow.getMinutes()
+  const seconds = dateNow.getSeconds()
+  const weekDay = dateNow.getDay()
+  // const hours = dateNow.getHours()
+  // const hours = dateNow.getHours()
+  // console.log(hours);
+  
 
   function updateClock() {
-    const dateNow = new Date().getTime();
-    const clock = getClock();
-   
+    dateNow = new Date()
+    console.log(hours);
+    if (hours >= 18) {
+      timeOfDay.textContent = `ый вечер`;
+    }
+    if (hours >= 12 && hours < 18) {
+      timeOfDay.textContent = `ый день`;
+    }
+    if (hours >= 6 && hours < 12) {
+      timeOfDay.textContent = `ое утро`;
+    }
+    if (hours >= 0 && hours < 6) {
+      timeOfDay.textContent = `ой ночи`;
+    };
 
-    if (clock.hours > 12) {
-      clock.hours -= 12;
-      timeHours.textContent = clock.hours;
+    if (hours > 12) {
+      hours = hours - 12;
+      timeHours.textContent = hours;
       timing.textContent = ` PM `;
     };
-    
-    if (clock.hours < 10) {
-      timeHours.textContent = `0${clock.hours}`;
+
+    if (hours < 10) {
+      timeHours.textContent = `0${hours}`;
     } else {
-      timeHours.textContent = clock.hours;
+      timeHours.textContent = hours;
     };
-    console.log(clock.hours, clock.minutes, clock.seconds);
-    if (clock.minutes < 10) {
-      timeMinutes.textContent = `0${clock.minutes}`;
+    console.log(hours, minutes, seconds);
+    if (minutes < 10) {
+      timeMinutes.textContent = `0${minutes}`;
     } else {
-      timeMinutes.textContent = clock.minutes;
+      timeMinutes.textContent = minutes;
     };
-   
-    if (clock.seconds < 10) {
-      timeSeconds.textContent = `0${clock.seconds}`;
+
+    if (seconds < 10) {
+      timeSeconds.textContent = `0${seconds}`;
     } else {
-      timeSeconds.textContent = clock.seconds;
+      timeSeconds.textContent = seconds;
     };
 
     setTimeout(updateClock, 1000);
   };
+  //   function getClock() {
+  //     const timeNow = dateNow / 1000;
+  //     const seconds = Math.floor(timeNow % 60);
+  //     const minutes = Math.floor(((timeNow - seconds) / 60) % 60);
+  //     const hours = Math.floor((timeNow / 60 / 60) % 24);
+  //     return {
+  //       timeNow,
+  //       hours,
+  //       minutes,
+  //       seconds
+  //     };
+  //   };
 
-  function clockOf(newYear) {
-    const dateNewYear = new Date(newYear).getTime();
-    const action = Math.floor((dateNewYear - dateNow)/1000 / 24);
-    if (action < 10) {
-      countDay.textContent = `0${action}`;
-    } else {
-      countDay.textContent = `${action}`
-    };
-  };
-  
-  clockOf(`01 january 2020`)
-updateClock();
+  //   getClock();
+  //   function getTimeOfDay (){
+  //     const clock = getClock();
+  //     
+  //   }
+
+
+  //   function clockOf(newYear) {
+  //     const dateNewYear = new Date(newYear).getTime();
+  //     const action = Math.floor((dateNewYear - dateNow)/1000 / 24);
+  //     if (action < 10) {
+  //       countDay.textContent = `0${action}`;
+  //     } else {
+  //       countDay.textContent = `${action}`
+  //     };
+  //   };
+
+  //   clockOf(`01 january 2020`)
+  updateClock();
 
 });
